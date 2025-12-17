609cd2a57e1c1f0bb937f5ab00b7e2d954b39379
tanmayjai123 <tammayjaiswal@gmail.com>
2025-12-17 16:46:13 +0530

    Add welcome alert and dynamic text change

---
+++ script.js
@@ -0,0 +1,8 @@
+// Show message when site loads
+alert("Welcome to Tanmay Jaiswal's Portfolio 🚀");
+
+// Change text dynamically
+document.addEventListener("DOMContentLoaded", () => {
+  const p = document.querySelector("p");
+  p.innerText = "Computer Science Student | Web Developer";
+});
